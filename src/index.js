3fcca34fb523e5e9c92c5772167cc5758ce7da22
Juan <juan@charityhowto.com>
2024-03-25 12:18:30 -0500

    Downgrade react version for testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+/* Revisado desde la documentación oficial.
+ Asi se usaba en la versión 17 */
+import { render } from 'react-dom';
+const container = document.getElementById('root');
+render(<App tab="home" />, container);
